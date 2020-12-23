f30be5682fe44405c0708134723a34b176e2aa77
Abraham Perez <aperezpe01@gmail.com>
2020-12-23 12:19:35 -0600

    add save button to set interests page

--- lib/ui/screens/interests/set_interests_page.dart
+++ lib/ui/screens/interests/set_interests_page.dart
@@ -1,6 +1,5 @@
 import 'package:bonobo/services/database.dart';
 import 'package:bonobo/ui/common_widgets/bottom_button.dart';
-import 'package:bonobo/ui/common_widgets/custom_button.dart';
 import 'package:bonobo/ui/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:bonobo/ui/screens/interests/models/set_interests_page_model.dart';
 import 'package:bonobo/ui/screens/interests/widgets/clickable_box.dart';
@@ -25,6 +24,8 @@ class SetInterestsPage extends StatelessWidget {
     @required Friend friend,
     @required FirestoreDatabase database,
     @required List<SpecialEvent> friendSpecialEvents,
+    @required bool isNewFriend,
+    @required List<SpecialEvent> onDeleteSpecialEvents,
   }) async {
     await Navigator.of(context).push(
       MaterialPageRoute(
@@ -32,7 +33,9 @@ class SetInterestsPage extends StatelessWidget {
           create: (context) => SetInterestsPageModel(
             database: database,
             friend: friend,
+            isNewFriend: isNewFriend,
             friendSpecialEvents: friendSpecialEvents,
+            onDeleteSpecialEvents: onDeleteSpecialEvents,
           ),
           child: Consumer<SetInterestsPageModel>(
             builder: (context, model, __) => SetInterestsPage(
@@ -44,7 +47,7 @@ class SetInterestsPage extends StatelessWidget {
     );
   }
 
-  Future<void> submit(BuildContext context) async {
+  Future<void> _submit(BuildContext context) async {
     try {
       await model.submit();
       Navigator.popUntil(context, (route) => route.isFirst);
@@ -61,11 +64,23 @@ class SetInterestsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Interests"),
+        actions: model.isNewFriend
+            ? []
+            : [
+                FlatButton(
+                  child: Text(
+                    'Save',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                  // TODO: Snackbar or something when is not ready to submit
+                  onPressed: isReadyToSubmit ? () => _submit(context) : null,
+                ),
+              ],
       ),
       body: _buildContent(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: BottomButton(
-        onPressed: isReadyToSubmit ? () => submit(context) : null,
+        onPressed: isReadyToSubmit ? () => _submit(context) : null,
         color: isReadyToSubmit ? Colors.orange[600] : Colors.grey,
         text: model.submitButtonText,
         textColor: isReadyToSubmit ? Colors.black : Colors.white,
