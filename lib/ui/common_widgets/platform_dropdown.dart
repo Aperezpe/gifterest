6251ef1ea34abb0f111b972eba6790ab6d0d58c2
Abraham <aperezpe01@gmail.com>
2021-02-11 09:35:45 -0600

    platform dropdown first commit

---
+++ lib/ui/common_widgets/platform_dropdown.dart
@@ -0,0 +1,14 @@
+import 'package:bonobo/ui/common_widgets/platform_widget.dart';
+import 'package:flutter/cupertino.dart';
+
+class PlatformDropdown extends PlatformWidget {
+  @override
+  Widget buildCupertinoWidget(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildMaterialWidget(BuildContext context) {
+    return Container();
+  }
+}
