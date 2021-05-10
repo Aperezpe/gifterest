e7e0a5477764a203d8d9dd0e309c30329ae4808a
Abraham <aperezpe01@gmail.com>
2021-05-10 13:26:23 -0500

    makes setup page responsive

--- lib/ui/screens/profile_setup/setup_page.dart
+++ lib/ui/screens/profile_setup/setup_page.dart
@@ -1,3 +1,4 @@
+import 'package:bonobo/resize/size_config.dart';
 import 'package:bonobo/ui/common_widgets/gradient_button.dart';
 import 'package:bonobo/ui/models/person.dart';
 import 'package:bonobo/ui/screens/profile_setup/gender_page.dart';
@@ -9,77 +10,105 @@ class SetupPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
+
+    final is800Hight = SizeConfig.screenHeight >= 800;
+    final is700Wide = SizeConfig.screenWidth >= 700;
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: RadialGradient(
-                colors: [Color(0xffFB997F), Color(0xffF47199)],
-                radius: 1.3,
-                center: Alignment(0, -.4)),
-          ),
-          padding: EdgeInsets.fromLTRB(20, 150, 20, 80),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Column(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width / 2,
-                    child: Image.asset('assets/carita.png'),
-                  ),
-                  SizedBox(height: 80),
-                  Text(
-                    "Setup your profile",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 36,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Montserrat',
-                        color: Colors.white,
-                        shadows: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(.25),
-                            offset: Offset(1, 4),
-                            blurRadius: 4,
-                          ),
-                        ]),
-                  ),
-                  SizedBox(height: 50),
-                  Padding(
-                    padding: EdgeInsets.only(left: 20, right: 20),
-                    child: Text(
-                      "Allow us to ask you 3 questions and we'll give you better recommendations",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontFamily: 'Mulish',
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+              colors: [Color(0xffFB997F), Color(0xffF47199)],
+              radius: 1.3,
+              center: Alignment(0, -.4)),
+        ),
+        padding: EdgeInsets.only(
+          top: SizeConfig.blockSizeVertical * 5,
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 6,
+              child: Container(
+                width: MediaQuery.of(context).size.width / 2,
+                child: Image.asset('assets/carita.png'),
+              ),
+            ),
+            Expanded(flex: 1, child: SizedBox()),
+            Expanded(
+              flex: 1,
+              child: Text(
+                "Setup your profile",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: SizeConfig.safeBlockVertical * 5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Montserrat',
+                    color: Colors.white,
+                    shadows: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(.25),
+                        offset: Offset(1, 4),
+                        blurRadius: 4,
                       ),
+                    ]),
+              ),
+            ),
+            Expanded(flex: 1, child: SizedBox()),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: SizeConfig.safeBlockHorizontal * 8,
+                  right: SizeConfig.safeBlockHorizontal * 8,
+                ),
+                child: Text(
+                  "Allow us to ask you 3 questions and we'll give you better recommendations",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: SizeConfig.safeBlockVertical * 2.5,
+                    fontFamily: 'Mulish',
+                  ),
+                ),
+              ),
+            ),
+            Expanded(flex: 1, child: SizedBox()),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: SizeConfig.blockSizeHorizontal * 5,
+                  right: SizeConfig.blockSizeHorizontal * 5,
+                  top: is800Hight
+                      ? SizeConfig.safeBlockVertical * 3
+                      : SizeConfig.blockSizeVertical * 2.2,
+                  bottom: is800Hight
+                      ? SizeConfig.safeBlockVertical * 2.5
+                      : SizeConfig.blockSizeVertical * 1.2,
+                ),
+                child: GradientButton(
+                  text: "Let's Start",
+                  textSize: is700Wide
+                      ? SizeConfig.safeBlockVertical * 2.5
+                      : SizeConfig.safeBlockVertical * 2.8,
+                  onPressed: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => GenderPage(user: user),
                     ),
                   ),
-                  SizedBox(height: 30),
-                ],
-              ),
-              GradientButton(
-                text: "Let's Start",
-                onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => GenderPage(user: user),
+                  textColor: Colors.white,
+                  gradient: LinearGradient(
+                    colors: <Color>[Color(0xff00E9D0), Color(0xff00AD87)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
                   ),
+                  shadowColor: Colors.black.withOpacity(.25),
                 ),
-                textColor: Colors.white,
-                gradient: LinearGradient(
-                  colors: <Color>[Color(0xff00E9D0), Color(0xff00AD87)],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-                shadowColor: Colors.black.withOpacity(.25),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
