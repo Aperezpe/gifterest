244cf8eba88e55b7d63d2aca534ba44684580cb0
Abraham <aperezpe01@gmail.com>
2021-06-03 12:04:44 -0500

    use bouncing circle loading widget to improve ui

--- lib/ui/common_widgets/loading_screen.dart
+++ lib/ui/common_widgets/loading_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animations/loading_animations.dart';
 
 class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
+    return Scaffold(
+      body: Center(
         child: Container(
           color: Colors.white,
-          child: CircularProgressIndicator(),
+          child: LoadingBouncingGrid.circle(backgroundColor: Colors.pink),
         ),
       ),
     );
