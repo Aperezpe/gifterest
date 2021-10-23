658a0b52f400c68498d6317aad9c7e3b74f3ea5e
Abraham Perez - a0p0cdd <Abraham.Perez0@walmart.com>
2021-10-23 16:31:56 -0500

    disable HttpOverrides to push to prod

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ import 'package:provider/provider.dart';
 import 'ui/screens/landing/landing_page.dart';
 
 void main() async {
-  HttpOverrides.global = new MyHttpOverrides(); // Only use in DEV
+  // HttpOverrides.global = new MyHttpOverrides(); // Only use in DEV
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   setupLocator();
@@ -42,11 +42,11 @@ class MyApp extends StatelessWidget {
 ///the aim of this answer is to make the development a bit easier for you, for production, you need to
 ///fix your certificate issue and use it properly, look at the other answers for this as it might be helpful for your case.
 ///https://stackoverflow.com/questions/54285172/how-to-solve-flutter-certificate-verify-failed-error-while-performing-a-post-req
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-  }
-}
+// class MyHttpOverrides extends HttpOverrides {
+//   @override
+//   HttpClient createHttpClient(SecurityContext context) {
+//     return super.createHttpClient(context)
+//       ..badCertificateCallback =
+//           (X509Certificate cert, String host, int port) => true;
+//   }
+// }
